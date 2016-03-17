8e14c2c2b721d4aa3e2eec6d42d1c66ad558a989
全力 <quanli@quanlideMacBook-Air.local>
2016-03-18 00:08:02 +0800

    微博详情滚动

--- mainThread.js
+++ mainThread.js
@@ -1,32 +1,32 @@
 //*************************
-//爬虫主进程
+//爬虫主进程casperjs
 //*************************
 
-var casper = require("casper").create();
+var casper = require("casper").create({
+    pageSettings:{
+        loadImages: false
+    }
+});
 var x = require('casper').selectXPath;
 var weibo = require('./tools/caspWeibo');
 var USER = '13267241477';
 var PASS = 'ql13530088648';
 var messages=[];
 
-casper.getMessages=function() {
-    var messages = document.querySelectorAll('p.default-content');
-    return Array.prototype.map.call(messages, function(e) {
-        return e.innerHTML;
-    });
-}
+
 
 casper.start('http://m.weibo.cn/',function() {
     weibo.login(USER,PASS);
 });
 
-casper.then(function() {
+casper.thenOpen('http://m.weibo.cn/u/2271625161',function() {
+    weibo.getMsg();
     //this.echo(this.fetchText('p.default-content'))
-    messages=messages.concat(this.evaluate(casper.getMessages));
+    //messages=messages.concat(this.evaluate(casper.getMessages));
 });
              
 casper.run(function() {
-    this.echo(messages.length+' messages found:');
-    this.echo(' - ' + messages.join('\n--------------------\n')).exit();
+    //this.echo(messages.length+' messages found:');
+    //this.echo(' - ' + messages.join('\n--------------------\n')).exit();
 });
 
--- tools/caspWeibo.js
+++ tools/caspWeibo.js
@@ -1,9 +1,46 @@
 //*************************
-//手机版微博登录
+//微博模块
 //*************************
 //var x = require('casper').selectXPath;
 
-casper.displayCookies=function()
+function getCurrentInfoNum() {return document.querySelectorAll('.card-list div.card').length;}
+
+casper.tryAndScroll=function() {
+    try{
+    this.echo('scroll!!');
+    this.scrollToBottom();
+    //var info = this.getElementInfo();
+    //this.wait(500);
+    if (this.exists('div.loading')) {
+        var curItems = this.evaluate(getCurrentInfoNum);
+        this.echo(curItems);
+        casper.waitFor(function check(){
+            return curItems != this.evaluate(getCurrentInfoNum);
+        }, function then(){
+            this.tryAndScroll();
+        }, function onTimeout(){
+            this.echo("Timout reached");
+        }, 20000);
+    } else {
+        this.echo("no more items");
+  }
+    }catch(err) {
+        this.echo(err);
+    }
+}
+
+casper.getTotalInfoNum = function() {return 185;
+    //return parseInt(document.querySelector('.layout-box a:nth-child(2) div:nth-child(1)').innerHTML);}
+                                    }
+
+function getMessages() {
+    var messages = document.querySelectorAll('p.default-content');
+    return Array.prototype.map.call(messages, function(e) {
+        return e.innerHTML;
+    });
+}//casper.getMessages
+
+casper.displayCookies=function() //显示当前cookies
 {
     console.log('---------------------------------------------------------------');
     var cookies = phantom.cookies;
@@ -13,12 +50,11 @@ casper.displayCookies=function()
     console.log('---------------------------------------------------------------');
 }//casper.displayCookies
 
-var login = function(USER,PASS)
+var login = function(USER,PASS) //微博登录
 {
-    //casper.start('http://m.weibo.cn/') 
-
+    //casper.start('http://m.weibo.cn/')
     casper.wait(3000, function() {
-        this.capture('weibo.png'); //网页打开页面截图
+        this.capture('./data/weibo.png'); //网页打开页面截图
         this.echo(this.getTitle());
         this.click(x('/html/body/div/div/a[2]')); //点击登录
         casper.displayCookies(); 
@@ -33,12 +69,56 @@ var login = function(USER,PASS)
         this.click(x('//*[@id="loginAction"]'));  //点击登录按钮
     }); 
 
-    casper.wait(5000, function(){ 
+    casper.wait(2000, function(){
         this.echo(this.getCurrentUrl());  //登录成功标志
-        this.capture("login.png"); 
+        this.capture("./data/login.png");
         casper.displayCookies();
     }); 
+    return true;
     
 }//caspWeibo.login
 
+var getMsg = function() //获取微博消息
+{
+    try{
+    var num = casper.getTotalInfoNum();
+    } catch(err) {
+        console.log(err);
+    }
+    console.log(num);
+    casper.waitForText('查看更多微博',function() {
+        this.capture('./data/mainPage.png');
+        this.click('.more-detail a.mct-d');
+        this.wait(2000);
+    }).then(function() {
+        this.echo('getmsg');
+        this.capture('./data/detail.png');
+    });
+    casper.then(function() {
+        try {
+       casper.tryAndScroll();
+        } catch(err) {
+            this.echo(err);
+        }
+    });
+    casper.wait(5000,function() {
+       this.capture('./data/afterdetail.png');
+    });
+}
+
+//var getUsers = function() //获取用户信息
+//{
+//    casper.then(function() {
+//
+//    });
+//}
+//
+//var getMsgComments = function() //获取微博评论
+//{
+//    casper.then(function() {
+//
+//    });
+//}
+
 exports.login = login;
+exports.getMsg = getMsg;
