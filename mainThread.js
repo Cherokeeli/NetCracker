f02d40eb72cb625c373bf9038a01f65b446b4bcd
全力 <quanli@quanlideMacBook-Air.local>
2016-04-15 00:07:11 +0800

    加入UserAgent伪装标识库

--- mainThread.js
+++ mainThread.js
@@ -2,11 +2,26 @@
 //爬虫主进程casperjs
 //*************************
 
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+var userAgentPool = [
+    'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+    'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+    'Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+    'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11',
+    'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
+    'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5'
+];
+
 var msgPage = require("casper").create({
     pageSettings: {
         loadImages: false,
         loadPlugins: false,
-        userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X; en-us) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53'
+        userAgent: userAgentPool[getRandomArbitrary(0,8)]
     }
 //        ,
 //        verbose: true,
@@ -18,7 +33,7 @@ var focusPage = require("casper").create({
     pageSettings: {
         loadImages: false,
         loadPlugins: false,
-        userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X; en-us) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53'
+        userAgent: userAgentPool[getRandomArbitrary(0,8)]
     }
 //        ,
 //        verbose: true,
@@ -30,7 +45,7 @@ var userPage = require("casper").create({
     pageSettings: {
         loadImages: false,
         loadPlugins: false,
-        userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X; en-us) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53'
+        userAgent: userAgentPool[getRandomArbitrary(0,8)]
     }
 //        ,
 //        verbose: true,
