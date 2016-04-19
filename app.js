3fc8d329ade036693da8512ac0d254b7dffd2466
全力 <quanli@quanlideMacBook-Air.local>
2016-04-19 20:32:52 +0800

    app.js入口文件调用爬虫

--- app.js
+++ app.js
@@ -6,22 +6,27 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
+var scraper = require('./ControlCenter');
+var cook = require('./CookieCollector');
 var request = require('request');
 
 
 var app = express();
 
-app.get('/',function(req,res) {
-    request('http://m.weibo.cn/',function(error,response,body) {
-        if(!error && response.statusCode == 200) {
-            console.log(body);
-            //res.send(body);
-            res.send('End Crawl,Please check the terminal');
-        }
-    });
-});
+setInterval(cook.updateCookies,10*(60*60*1000));
 
-app.listen(4000,function(){
-    console.log('listening on 4000');
-});
+scraper.start();
+
+//app.get('/',function(req,res) {
+//    request('http://m.weibo.cn/',function(error,response,body) {
+//        if(!error && response.statusCode == 200) {
+//            console.log(body);
+//            //res.send(body);
+//            res.send('End Crawl,Please check the terminal');
+//        }
+//    });
+//});
+//
+//app.listen(4000,function(){
+//    console.log('listening on 4000');
+//});
